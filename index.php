c00ae030c620cdc34f62feb8ed4cd93854b1287c
donatasju <donatas.jurkusl@gmail.com>
2019-06-04 15:22:45 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once 'bootstrap.php'; ?>
+<html>
+    <head>
+        <title>BALLERS | Welcome!</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <!-- Navigation -->    
+        <?php require 'objects/navigation.php'; ?>        
+
+        <!-- Content -->   
+        <h1>Welcome to BALLERS!</h1>
+    </body>
+</html>
